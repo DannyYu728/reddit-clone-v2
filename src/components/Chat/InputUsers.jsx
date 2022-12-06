c0edb784362eb3dbf16b9459f34dfc9aca28759e
Danny Yu <tiramisu@Dannys-MBP.home>
2022-12-05 20:13:55 -0500

    chat

--- src/components/Chat/InputUsers.jsx
+++ src/components/Chat/InputUsers.jsx
@@ -6,7 +6,7 @@ function InputUsers({ users, handleSelectedUser, setUsernameInput }) {
     <div className="chat-search-users-container">
       {users &&
         users.map((user) => {
-          if (user.username && user.avatar) {
+          if (user.username) {
             return (
               <div
                 key={user.id}
@@ -17,7 +17,7 @@ function InputUsers({ users, handleSelectedUser, setUsernameInput }) {
                 }}
               >
                 <img
-                  src={user.avatar}
+                  src="https://dy-reddit-v2.up.railway.app/static/default.png"
                   className="chat-search-user-avatar"
                 ></img>
                 <p>{user.username}</p>
